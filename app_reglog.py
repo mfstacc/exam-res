76ad98d9ec34e56c8a8e54bfa17d96899f91708e
m0xn <moon0xcoder@gmail.com>
2024-03-13 21:57:58 +0100

    Removed problematic variable and made the model better

--- app_reglog.py
+++ app_reglog.py
@@ -12,13 +12,12 @@ with st.sidebar:
     pregancies = st.slider(label='pregnancies', min_value=0, max_value=10, value=4)
     glucose = st.slider(label='glucose', min_value=120, max_value=300, value=122)
     blood_pressure = st.slider(label='blood_pressure', min_value=50, max_value=120, value=70)
-    skin_thickness = st.slider(label='skin_thickness', min_value=5, max_value=50, value=21)
     bmi = st.slider(label='bmi', min_value=10.0, max_value=45.0, value=32.19)
     dpf = st.slider(label='dpf', min_value=0.0, max_value=2.0, value=0.47)
     age = st.slider(label='age', min_value=10, max_value=100, value=33)
 
     predict_button = st.button(label='Predict', type='primary')
-    parameters = np.array([pregancies, glucose, blood_pressure, skin_thickness, bmi, dpf, age])
+    parameters = np.array([pregancies, glucose, blood_pressure, bmi, dpf, age])
 
 if predict_button:
     outcome = model.predict(parameters.reshape(1, -1))
